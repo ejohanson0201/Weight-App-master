d6a5c6e809baa1706496ced53848cf751a047628
period2 <period2@p-162mba-06.local>
2018-10-30 09:20:49 -0500

    Initial Commit

---
+++ Weight App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Weight App
+//
+//  Created by period2 on 10/29/18.
+//  Copyright © 2018 period2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
