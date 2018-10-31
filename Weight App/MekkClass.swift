603ec6814319e8055b710a6dd028c086429cd521
period2 <period2@p-162mba-06.d214.org>
2018-10-31 09:18:58 -0500

    original commit

---
+++ Weight App/MekkClass.swift
@@ -0,0 +1,14 @@
+//
+//  MekkViewController.swift
+//  Weight App
+//
+//  Created by period2 on 10/30/18.
+//  Copyright © 2018 period2. All rights reserved.
+//
+
+import UIKit
+
+class MekkClass()
+{
+
+}
